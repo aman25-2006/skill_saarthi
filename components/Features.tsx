3251b969317c1f825475df170482f85194836f66
Aman Kumar <asiaaman988@gmail.com>
2026-09-07 18:44:28 +0530

    Fix unused idx variable in Features component

--- components/Features.tsx
+++ components/Features.tsx
@@ -122,7 +122,7 @@ export default function Features() {
             variants={containerVariants}
             className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"
           >
-            {features.map((feature, idx) => {
+            {features.map((feature) => {
               const Icon = feature.icon;
               return (
                 <motion.div
